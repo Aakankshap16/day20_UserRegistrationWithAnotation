d1371232fa9777072730c186602b82873646d74e
Aakankshap16 <aakankshap971@gmail.com>
2023-04-10 01:44:03 +0530

    display UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace UserRegistrationWithAnotation
+﻿using UserRegistrartionWithAnotation;
+
+namespace UserRegistrationWithAnotation
 {
     class Program
     {
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to User Regestration problem!!");
+            Console.WriteLine("*********************************");
+
+            Annotest.Display();
         }
     }
 }
