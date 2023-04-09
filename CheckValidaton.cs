1e5f4f2768e8ff351130178d93536afabfe3dddd
Aakankshap16 <aakankshap971@gmail.com>
2023-04-10 01:59:04 +0530

    contain condition

--- CheckValidaton.cs
+++ CheckValidaton.cs
@@ -12,6 +12,9 @@ namespace UserRegistrartionWithAnotation
        // [Required(ErrorMessage = "Property check {0} is required")]
         [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
        // [StringLength(100,MinimumLength =3 ,ErrorMessage = "Name should be minimum 3 character" )]
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
+        public string LastName { get; set; }
     }
 }
